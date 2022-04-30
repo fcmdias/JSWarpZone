6ac03993e38cc6d892f6211aee23b5cb63f4753a
fcmdias <fernando.cesardias7@yahoo.com>
2022-04-30 12:36:06 +0100

    adding interpreter design pattern - lexing

---
+++ design_patterns/interpreter/2_parsing.js
@@ -0,0 +1,2 @@
+// interpreter is a component that processes structured text data. Does so by turning it into seperate lexial tokens (lexing) and then interpreting sequences of said tokens (parsing)
+
